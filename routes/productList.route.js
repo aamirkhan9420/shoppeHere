f7f2c6fe0c167c582949760a0706f0a1c4a748ce
unknown <aamir.khan@actualpixel.in>
2025-08-20 16:01:38 +0530

    extra space removed

--- routes/productList.route.js
+++ routes/productList.route.js
@@ -140,23 +140,16 @@ ProductListRouter.put('/update-favourite', async (req, res) => {
     });
   }
 });
-
-
-
-
 ProductListRouter.get('/', async (req, res) => {
   try {
     const { gender, sort, color, size, type } = req.query;
-
     let filter = {};
-
     if (gender) filter.gender = gender.toLowerCase(); // male/female
     if (type) filter.type = type; // Suits, Shirts, etc.
     if (color) filter.color = { $in: [color.toLowerCase()] }; // matches if color is in array
     if (size) filter.size = { $in: [size.toUpperCase()] };   // matches if size is in array
 
     let query = ProductListModel.find(filter);
-
     // Sorting
     if (sort === 'highPrice') {
       query = query.sort({ price: -1 }); // highest first
